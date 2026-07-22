0a138ccaf8564d5d81ba3e26be903705cad82770
Gabriel Timon <bieltimon@gmail.com>
2026-07-22 12:20:00 -0300

    criei algumas rotas

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello World!")
+from fastapi import FastAPI
+
+materias = []
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "Bem vindo(a) ao Organiador de Estudos"
+
+
+@app.post("/meterias")
+def create_materia(materia: str):
+    materias.append(materia)
+
+
+@app.get("/materias")
+def get_materias():
+    return materias
+
+
+@app.get("/materias/{id}")
+def get_materiaByID(id: int) -> str:
+    item = materias[id]
+    return item
