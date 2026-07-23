c488dc2ee049926131f3d42e42cfe14a2cc07418
Gabriel Timon <bieltimon@gmail.com>
2026-07-23 12:04:18 -0300

    add: extend Materia model with descricao, cor, and data_criacao fields

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
 from app.database import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Integer
+from datetime import datetime
 
 class Materia(Base):
     __tablename__ = "materias"
     nome: Mapped[str]
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    descricao: Mapped[str | None]
+    cor: Mapped[str | None]
+    data_criacao: Mapped[datetime] = mapped_column(default=datetime.now)
+    
