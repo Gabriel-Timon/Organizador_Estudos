0932a1da91c0a410ce58a62cd1dc643e248e776a
Gabriel Timon <bieltimon@gmail.com>
2026-07-24 13:45:10 -0300

    add: implement field validator for nome in CreateMateria schema to ensure non-empty values

--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,17 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, field_validator
 from datetime import datetime
 
 class CreateMateria(BaseModel):
     nome: str
     descricao: str | None = None
     cor: str | None = None
+
+    @field_validator('nome')
+    @classmethod
+    def tratar_string_vazia(cls, nome: str):
+        if nome.strip() == "":
+            raise ValueError("O nome não pode ser vazio.")
+        return nome.strip()
 
 
 class GetMateria(BaseModel):
