aab220b7ab69a78a5afb81a4ef337acf64320ec4
Gabriel Timon <bieltimon@gmail.com>
2026-07-24 14:32:27 -0300

    add: enhance Tarefa model and CreateTarefa schema with validation for titulo and additional fields

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app.database import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer
-from datetime import datetime
+from sqlalchemy import Integer, ForeignKey
+from datetime import datetime, date
 
 class Materia(Base):
     __tablename__ = "materias"
@@ -10,3 +10,17 @@ class Materia(Base):
     descricao: Mapped[str | None]
     cor: Mapped[str | None]
     data_criacao: Mapped[datetime] = mapped_column(default=datetime.now)
+
+
+class Tarefa(Base):
+    __tablename__ = "tarefas"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    materia_id: Mapped[int] = mapped_column(ForeignKey("materias.id"))
+    titulo: Mapped[str]
+    descricao: Mapped[str | None]
+    data_limite: Mapped[date | None]
+    data_criacao: Mapped[datetime] = mapped_column(default=datetime.now)
+    data_conclusao: Mapped[datetime | None]
+    prioridade: Mapped[str]
+    status: Mapped[str] = mapped_column(default="pendente")
+
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, ConfigDict, field_validator
-from datetime import datetime
+from datetime import datetime, date
+from typing import Literal
 
 class CreateMateria(BaseModel):
     nome: str
@@ -8,9 +9,9 @@ class CreateMateria(BaseModel):
 
     @field_validator('nome')
     @classmethod
-    def tratar_string_vazia(cls, nome: str):
+    def tratar_string_vazia(cls, nome: str) -> str:
         if nome.strip() == "":
-            raise ValueError("O nome não pode ser vazio.")
+            raise ValueError("O nome da matéria não pode ser vazio.")
         return nome.strip()
 
 
@@ -22,3 +23,32 @@ class GetMateria(BaseModel):
     descricao: str | None
     cor: str | None
     data_criacao: datetime
+
+
+class CreateTarefa(BaseModel):
+    titulo: str
+    descricao: str | None = None
+    materia_id: int
+    prioridade: Literal["baixa", "media", "alta"]
+    data_limite: date | None = None
+
+    @field_validator("titulo")
+    @classmethod
+    def tratar_string_vazia(cls, titulo: str) -> str:
+        if titulo.strip() == "":
+            raise ValueError("O título da tarefa não pode ser vazio.")
+        return titulo.strip()
+
+
+class GetTarefa(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    titulo: str
+    descricao: str | None
+    materia_id: int
+    prioridade: Literal["baixa", "media", "alta"]
+    data_limite: date | None
+    status: Literal["pendente", "em_andamento", "concluida"]
+    data_criacao: datetime
+    data_conclusao: datetime | None
